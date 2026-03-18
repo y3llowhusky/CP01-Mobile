6f7def1c113485c797f04add1aaffc0186b24436
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-18 08:58:21 -0300

    feat: added text inputs for form

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+  const[nomeAluno, setNomeAluno]=useState("");
+  const[cursoAluno, setCursoAluno]=useState("");
+  const[disciplinaAluno, setDisciplinaAluno]=useState("");
+  const[descricaoAluno, setDescricaoAluno]=useState("");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput placeholder="Digite seu nome"/>
+      <TextInput placeholder="Digite seu curso"/>
+      <TextInput placeholder="Digite sua disciplina"/>
+      <TextInput placeholder="Digite sua descrição"/>
+    </SafeAreaView>
   );
 }
 
