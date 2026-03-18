2e272f9f89b61b2b798689cb0aa244b8e8943c93
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-18 08:52:35 -0300

    feat: project skeleton

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
---
+++ src/screens/Review.jsx
@@ -0,0 +1,14 @@
+import { Text,View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function Review({nomeAluno, cursoAluno, disciplinaAluno, descricaoAluno}) {
+    return (
+        <View>
+            <Text>Visualizando formulário pronto...</Text>
+            <Text>Nome: {nomeAluno}</Text>
+            <Text>Curso: {cursoAluno}</Text>
+            <Text>Disciplina: {disciplinaAluno}</Text>
+            <Text>Descrição: {descricaoAluno}</Text>
+        </View>
+    )
+}
