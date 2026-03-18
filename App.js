50413538f495be6b72f2fe120fa218584b7e7f83
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-18 09:19:10 -0300

    feat: added textinputs for form fields and button for visualization

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, Button, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Review from './src/screens/Review';
 
 export default function App() {
   const[nomeAluno, setNomeAluno]=useState("");
   const[cursoAluno, setCursoAluno]=useState("");
   const[disciplinaAluno, setDisciplinaAluno]=useState("");
   const[descricaoAluno, setDescricaoAluno]=useState("");
+  const[mostrarDados, setMostrarDados]=useState(false);
 
   return (
     <SafeAreaView style={styles.container}>
-      <TextInput placeholder="Digite seu nome"/>
-      <TextInput placeholder="Digite seu curso"/>
-      <TextInput placeholder="Digite sua disciplina"/>
-      <TextInput placeholder="Digite sua descrição"/>
+      <Image/>
+
+      <TextInput placeholder="Digite seu nome" onChangeText={setNomeAluno}/>
+      <TextInput placeholder="Digite seu curso" onChangeText={setCursoAluno}/>
+      <TextInput placeholder="Digite sua disciplina" onChangeText={setDisciplinaAluno}/>
+      <TextInput placeholder="Digite sua descrição" onChangeText={setDescricaoAluno}/>
+
+      <Button 
+        title="Enviar dados" 
+        onPress={()=>{setMostrarDados(!mostrarDados)}}
+      />
+
+      {mostrarDados && <Review nomeAluno={nomeAluno} cursoAluno={cursoAluno} 
+      disciplinaAluno={disciplinaAluno} descricaoAluno={descricaoAluno}/>}
     </SafeAreaView>
   );
 }
@@ -26,4 +38,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{resizeMode:"center"},
+  input:{
+    backgroundColor:"#ccc",
+    width:300,
+    borderRadius:10
+  }
 });
